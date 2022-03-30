ef01727dfa0f960a8d57196f02ba275ce20416d4
William Hagan <williamhagan1728@yahoo.com>
2022-03-30 15:20:26 -0700

    import components into app.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 
+import React, { useState } from 'react';
+import DisplayPosts from './Components/DisplayPosts';
+import NavBar from './Components/NavBar';
+import CreatePost from './Components/CreatePost';
+function App() {
+  const [posts, setPosts] = useState([
+    { name: "Matthew Bshero", comment: "Test post 1" },
+    { name: "Matthew Bshero", comment: "Test post 2" },
+  ]);
+
+  function addNewPost(post){
+    let tempPosts = [...posts, post];
+    setPosts(tempPosts);
+  }
+
+  return (
+    <div>
+      <NavBar />
+      <main>
+      <CreatePost />
+      <CreatePost addNewPostProp={addNewPost}/>
+      <DisplayPosts post={posts}/>
+      </main>
+    </div>
+  );
+}
+export default App;
