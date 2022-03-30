ec080b3a200f078702ea5dca76626c98e26eab39
William Hagan <williamhagan1728@yahoo.com>
2022-03-29 17:23:02 -0700

    Create components files/folders for navbar, posts, create post

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+const NavBar = (props) => {
+    return ( 
+        <header>
+            <h1>Bshero<small className="text-muted">Social</small></h1>
+            <h1 className="text-black">Bshero<small className="text-muted">Social</small></h1>
+        </header>
+     );
+}
+ 
+export default NavBar;
---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,52 @@
+import { FaThumbsDown } from 'react-icons/fa';
+import { FaThumbsUp } from 'react-icons/fa';
+import React, { useState } from 'react';
+import './Post.css';
+
+const Post = (props) => {
+const [likeactive, setLikeactive] = useState(false);
+const [dislikeactive, setDislikeactive] = useState(false);
+const today = new Date();
+
+function likef(){
+  if (likeactive){
+    setLikeactive(false)
+  } else{
+    setLikeactive(true)
+    if(dislikeactive){
+      setDislikeactive(false)
+    }
+  }
+}
+function dislikef(){
+  if (dislikeactive){
+    setDislikeactive(false)
+  } else{
+    setDislikeactive(true)
+    if(likeactive){
+      setLikeactive(false)
+    }
+  }
+}
+
+  return (
+    <td>
+      <div>
+        <h3 className="col">{props.post.name}</h3>
+        <p className="col">{props.post.comment}</p>
+        <p><small>{today.toUTCString()}</small></p>
+        <p><small>{today.toDateString()}</small></p>
+        <div className='pull-right'>
+        <button className={[likeactive ? 'btn btn-outline-success btn-lg': 'btn btn-lg' ].join('')} onClick={likef}>
+        <FaThumbsUp />
+        </button>
+        <button className={[dislikeactive ? 'btn btn-outline-danger btn-lg': 'btn btn-lg' ].join('')} onClick={dislikef} >
+        <FaThumbsDown />
+        </button>
+        </div>
+      </div>
+    </td>
+  );
+};
+
+export default Post;
