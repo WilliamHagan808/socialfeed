da20800de423a6add127b4ef2e91669a042777b3
William Hagan <williamhagan1728@yahoo.com>
2022-03-29 17:08:25 -0700

    create components folder with jsx code

---
+++ src/components/DisplayPosts/DisplayPosts.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Post from "../PostFolder/Post";
+import './DisplayPosts.css'
+
+
+const DisplayPosts = (props) => {
+  return (
+    <table className='table'>
+      <thead className='thread-dark'>
+        <tr>
+          <th>Social Feed</th>
+        </tr>
+      </thead>
+      <tbody>
+              {props.post.map((post, index) => {
+        return (
+            <tr key={index}>
+            <Post post={post} />
+            </tr>
+        );
+      })
+    }
+      </tbody>
+    </table>
+  );
+};
+export default DisplayPosts;
